4d1bfb9ff659d46b26a44adf288518543f8c6ffc
Juho <37773658+JuhoKon@users.noreply.github.com>
2018-04-02 19:28:38 +0300

    Update tuloslista.h
    #include guards

--- tuloslista.h
+++ tuloslista.h
@@ -1,19 +1,20 @@
 /*****************************************************************************/
 /* CT60A0220 C-ohjelmoinnin ja testauksen periaatteet 
- * Tekij‰: Juho Kontiainen
+ * Tekij√§: Juho Kontiainen
  * Opiskelijanumero: 0503209
- * P‰iv‰m‰‰r‰: 06.03.2018
- * Yhteistyˆ ja l‰hteet, nimi ja yhteistyˆn muoto: harjoitusryhmiss‰ tyˆskentely, cprogramming.com, stackoverflow, 
+ * P√§iv√§m√§√§r√§: 06.03.2018
+ * Yhteisty√∂ ja l√§hteet, nimi ja yhteisty√∂n muoto: harjoitusryhmiss√§ ty√∂skentely, cprogramming.com, stackoverflow, 
  */
 /*****************************************************************************/
-#include "nimilista.h"
+#ifndef tuloslista_included
+#define tulosista_included
 struct Node2 {
 	int keskiarvo, minpituus, maxpituus, muistip, rivimaara, muistim;
 	char nimi2[30];
 	struct Node2 *pNext2;
 };
 typedef struct Node2 Solmu2;
-
+#endif /*tuloslista.h*/
 Solmu2* tyhjennatulos(Solmu2 *pAlku2);
 void tulostakaikki(Solmu2 *pAlku2);
 void tallennus(Solmu2 *pAlku2);
