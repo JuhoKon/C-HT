22c4a865040894f2a7ce27d41c43d0b7de2c3038
Juho <37773658+JuhoKon@users.noreply.github.com>
2018-04-02 19:27:30 +0300

    Update nimilista.h
    #define guard

--- nimilista.h
+++ nimilista.h
@@ -1,18 +1,20 @@
 /*****************************************************************************/
 /* CT60A0220 C-ohjelmoinnin ja testauksen periaatteet 
- * Tekij‰: Juho Kontiainen
+ * Tekij√§: Juho Kontiainen
  * Opiskelijanumero: 0503209
- * P‰iv‰m‰‰r‰: 06.03.2018
- * Yhteistyˆ ja l‰hteet, nimi ja yhteistyˆn muoto: harjoitusryhmiss‰ tyˆskentely, cprogramming.com, stackoverflow, 
+ * P√§iv√§m√§√§r√§: 06.03.2018
+ * Yhteisty√∂ ja l√§hteet, nimi ja yhteisty√∂n muoto: harjoitusryhmiss√§ ty√∂skentely, cprogramming.com, stackoverflow, 
  */
 /*****************************************************************************/
+#ifndef nimilista_included
+#define nimilista_included
 struct Node {
 	char nimi[30];
 	long int luku;
 	struct Node *pNext;
 };
 typedef struct Node Solmu;
-
+#endif /*nimilista.h*/
 
 Solmu* tdstoluku(Solmu *pAlku);
 void tulostalista(Solmu *pAlku);
